afcffb8fb0d5c8691da784f0782fcc2b43e57a7b
mgood7123 <smallville7123@gmail.com>
2020-07-08 21:29:36 +1000

    libsu - version 1.2

--- main.c
+++ main.c
@@ -4,10 +4,15 @@ int main() {
     libsu_processimage instance;
     int r;
     r = libsu_sudo(&instance, "ls /");
-    libsu_LOG_INFO("libsu returned: %d, stdout: %s", instance.return_code, instance.string_stdout);
+    libsu_LOG_INFO("libsu returned: %d", instance.return_code);
+    libsu_LOG_INFO("stdout: %s", instance.string_stdout);
+    libsu_LOG_INFO("stderr: %s", instance.string_stderr);
     libsu_cleanup(&instance);
+
     r = libsu_sudo(&instance, "true");
-    libsu_LOG_INFO("libsu returned: %d, stdout: %s", instance.return_code, instance.string_stdout);
+    libsu_LOG_INFO("libsu returned: %d", instance.return_code);
+    libsu_LOG_INFO("stdout: %s", instance.string_stdout);
+    libsu_LOG_INFO("stderr: %s", instance.string_stderr);
     libsu_cleanup(&instance);
     return 0;
 }
